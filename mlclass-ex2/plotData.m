b8e20c44889adbb768fd64224b14070c641fca72
Chris <bitcpf@gmail.com>
2013-11-30 16:19:02 -0600

    Couresa ML course

---
+++ mlclass-ex2/plotData.m
@@ -0,0 +1,28 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+index_pos=find(y==1);
+index_neg=find(y==0);
+
+
+plot(X(index_pos,1),X(index_pos,2),'k+','LineWidth',2,'MarkerSize',7);
+plot(X(index_neg,1),X(index_neg,2),'ko','MarkerFaceColor','y','MarkerSize',7);
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
